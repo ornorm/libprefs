a9b138e950630551a3a79737b6636440b8e503c6
biendo <aime@awelty.com>
2017-10-19 13:17:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# LIBPREFS
+> Manage shared preferences inside browser.
+
+Main classes: Preferences, PreferencesFactory.
+
+## Installation
+
+Node:
+
+```sh
+npm install libprefs --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,27 @@
+/** @babel */
+import {
+    DEFAULT_PREFERENCES_FILE_NAME,
+    LOCAL_STORAGE,
+    SESSION_STORAGE,
+    MAX_KEY_LENGTH,
+    MAX_VALUE_LENGTH,
+    MAX_NAME_LENGTH,
+    Base64,
+    PreferenceChangeEvent,
+    PreferenceChangeListener,
+    Preferences,
+    PreferencesFactory} from './lib/prefs';
+
+export {
+    DEFAULT_PREFERENCES_FILE_NAME,
+    LOCAL_STORAGE,
+    SESSION_STORAGE,
+    MAX_KEY_LENGTH,
+    MAX_VALUE_LENGTH,
+    MAX_NAME_LENGTH,
+    Base64,
+    PreferenceChangeEvent,
+    PreferenceChangeListener,
+    Preferences,
+    PreferencesFactory
+}
